34c85231c649f08ed45f36f3b701d693100be4f8
unknown <softdirex@gmail.com>
2018-08-17 23:19:16 -0300

    first commit

---
+++ pages/components/footer.php
@@ -0,0 +1,8 @@
+        <footer class="footer">
+          <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
+              <a href="http://www.softdirex.cl/" target="_blank">Softdirex</a>. Todos los derechos reservados.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Sistema de gestión CorpSec
+            </span>
+          </div>
+        </footer>
---
+++ pages/config/usuarios.php
@@ -0,0 +1,276 @@
+<?php
+/* Variables de uso general                            */
+$bg_sp=buildPath();
+$separator = "/";
+$navBar = "..".$separator."components".$separator."navbar.php";
+$sideBar = "..".$separator."components".$separator."sidebar.php";
+$footer = "..".$separator."components".$separator."footer.php";
+$title = "..".$separator."components".$separator."title.php";
+/***************************************************** */
+
+function buildPath(){
+  $domain =  'http://'.$_SERVER['HTTP_HOST'];
+  $subdomain = $_SERVER['PHP_SELF'];
+  $url = $domain.$subdomain;
+  $path = str_replace($domain.'/horizon', "", $url);
+  $slash = substr_count($path, '/')-1;
+  $buildPath = '';
+  $i = 0;
+  while ($i < $slash) {
+    $i++;
+    $buildPath = $buildPath."../";
+  }
+  return $buildPath;
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <?php include($title);?>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="<?php echo $bg_sp;?>vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="<?php echo $bg_sp;?>vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="<?php echo $bg_sp;?>vendors/css/vendor.bundle.addons.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="<?php echo $bg_sp;?>css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="<?php echo $bg_sp;?>images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <?php include($navBar); ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <?php include($sideBar);?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+        <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Lista de Usuarios</h4>
+                  
+                  <p class="card-description">
+                  <div class="form-group">
+                    Crear nuevo usuario
+                      <form action="nuevo.jsp"
+                            name="form1"
+                            id="form1"
+                            method="post"> 
+                            <input type="button" 
+                            class="btn btn-success btn-rounded btn-fw"
+                            value="Crear usuario" 
+                            id="nuevo"
+                            name="nuevo" 
+                            onclick= "document.form1.action = 'nuevo-usuario.php'; 
+                            document.form1.submit()" />
+                      </form>
+                      </div>
+                    <div class="form-group">
+                        <div class="input-group">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text">Buscar</span>
+                          </div>
+                          <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>
+                            User
+                          </th>
+                          <th>
+                            First name
+                          </th>
+                          <th>
+                            Progress
+                          </th>
+                          <th>
+                            Amount
+                          </th>
+                          <th>
+                            Deadline
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-1.png" alt="image" />
+                          </td>
+                          <td>
+                            Herman Beck
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $ 77.99
+                          </td>
+                          <td>
+                            May 15, 2015
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-2.png" alt="image" />
+                          </td>
+                          <td>
+                            Messsy Adam
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $245.30
+                          </td>
+                          <td>
+                            July 1, 2015
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-3.png" alt="image" />
+                          </td>
+                          <td>
+                            John Richards
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $138.00
+                          </td>
+                          <td>
+                            Apr 12, 2015
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-4.png" alt="image" />
+                          </td>
+                          <td>
+                            Peter Meggik
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $ 77.99
+                          </td>
+                          <td>
+                            May 15, 2015
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-1.png" alt="image" />
+                          </td>
+                          <td>
+                            Edward
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $ 160.25
+                          </td>
+                          <td>
+                            May 03, 2015
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-2.png" alt="image" />
+                          </td>
+                          <td>
+                            John Doe
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $ 123.21
+                          </td>
+                          <td>
+                            April 05, 2015
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="<?php echo $bg_sp;?>images/faces-clipart/pic-3.png" alt="image" />
+                          </td>
+                          <td>
+                            Henry Tom
+                          </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td>
+                            $ 150.00
+                          </td>
+                          <td>
+                            June 16, 2015
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <?php include($footer);?>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="<?php echo $bg_sp;?>vendors/js/vendor.bundle.base.js"></script>
+  <script src="<?php echo $bg_sp;?>vendors/js/vendor.bundle.addons.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="<?php echo $bg_sp;?>js/off-canvas.js"></script>
+  <script src="<?php echo $bg_sp;?>js/misc.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="<?php echo $bg_sp;?>js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+</body>
+
+</html>
